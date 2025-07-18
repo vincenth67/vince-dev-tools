6644952e1dd48a31165081e1ec40afa4737a0160
Vit Test User <vit.test@example.com>
2025-07-18 19:02:28 +0100

    fix(vit-init) fixing issue with -o not working in vit-init command

--- bin/vit.js
+++ bin/vit.js
@@ -5,5 +5,7 @@ const { routeCommand } = require('../lib/index.js');
 // Get command line arguments (skip first two: node path and script path)
 const args = process.argv.slice(2);
 
-// Route the command
-routeCommand(args); 
+// Route the command (support async)
+(async () => {
+  await require('../lib/index.js').routeCommand(args);
+})(); 
